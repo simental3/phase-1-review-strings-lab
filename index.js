8190012b1aca7bfe7095ae622b928bcceac209d2
Anthony Simental <anthonysimental12@gmail.com>
2022-11-29 16:35:34 -0800

    commit tree 4569eb9f4f917c517cdc559f6400edd7a37172ca

--- index.js
+++ index.js
@@ -1,1 +1,16 @@
 // Write your code in this file!
+const currentUser = 'Briana';
+
+
+const welcomeMessage = `Welcome to Flatbook, ${currentUser}!`;
+
+
+const excitedWelcomeMessage = `${welcomeMessage}`.toUpperCase();
+
+
+const shortGreeting = `Welcome, ${currentUser[0]}!`;
+
+
+console.log(welcomeMessage);
+console.log(excitedWelcomeMessage);
+console.log(shortGreeting);
